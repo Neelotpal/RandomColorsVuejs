cffc33289b1aee78370edaab82af5045e43c0c04
Neelotpal Patel <Neelotpal@users.noreply.github.com>
2018-01-05 17:19:52 +0530

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,57 @@
+<template>
+    <div id="app" class="centered" :style="{'width': width + 'px'}">
+            <four-child 
+                v-if="isMounted"
+                v-for="n in count"
+                :count="count"
+            ></four-child>
+            
+    </div>
+</template>
+
+<script>
+    import fourChild from './fourChild'
+    export default {
+        name: 'app',
+        components: {
+            'four-child': fourChild
+        },
+        data() {
+            return {
+                isMounted: false,
+                width: 100,
+                count: 1
+            }
+        },
+        mounted() {
+            this.isMounted = true;
+            setInterval(() => {
+                this.width += 10;
+                if (this.width % 100 === 0) {
+                    this.count = this.count * 4;
+                    if(this.count === 4096){
+                        this.count = 1;
+                    }
+                    if(this.width === 1000){
+                        this.width = 100;
+                    }
+                }
+            }, 300);
+    }
+    }
+</script>
+
+<style>
+    .centered {
+        position: fixed;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        -webkit-transform: translate(-50%, -50%);
+        -moz-transform: translate(-50%, -50%);
+        -o-transform: translate(-50%, -50%);
+        -ms-transform: translate(-50%, -50%);
+        padding: 5px;
+        z-index: 100;
+    }
+</style>
---
+++ src/fourChild.vue
@@ -0,0 +1,53 @@
+<template>
+   <div>
+        <div 
+            v-for="(color, index) in colors"    
+            class='square child1' 
+            :key="index"
+            :style="{'background-color': color, 'width': width + '%', 'padding-bottom': width + '%'}">
+        </div>
+
+    </div> 
+</template>
+
+<script>
+    export default {
+        props:['count'],
+        data(){
+            return {
+                colors:["aqua", "orchid", "blue", "yellow"]
+            }
+        },
+        computed:{
+            width(){
+                this.setColors();
+//                this.shuffleColors();
+                return 50 / Math.sqrt(this.count);
+            } 
+        },
+        methods:{
+            setColors(){
+                this.colors = this.colors.map(c => this.getRandomColor())
+            },
+            getRandomColor(){
+                return '#' + Math.floor(Math.random() * 16777215).toString(16);
+            },
+            shuffleColors(){
+                this.colors = this.colors.map(c => {
+                    let index = Math.floor(Math.random() * 4);
+                    return this.colors[index];
+                })
+            }
+        }
+    }
+</script>
+
+<style>
+.square {
+    float: left;
+    position: relative;
+    background: burlywood;
+}
+
+
+</style>
